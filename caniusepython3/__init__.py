056d8d2807bf621dad530c1316daefca48f1f47a
Marc Abramowitz <marc@marc-abramowitz.com>
2015-03-12 10:20:09 -0700

    Remove caniusepython3.check function
    It doesn't seem to be used AFAICT and it slows down running the tests,
    because it fetches all projects from PyPI.

--- caniusepython3/__init__.py
+++ caniusepython3/__init__.py
@@ -26,27 +26,3 @@ try:
     CPU_COUNT = max(2, multiprocessing.cpu_count())
 except NotImplementedError:  #pragma: no cover
     CPU_COUNT = 2
-
-
-def check(requirements_paths=[], metadata=[], projects=[]):
-    """Return True if all of the specified dependencies have been ported to Python 3.
-
-    The requirements_paths argument takes a sequence of file paths to
-    requirements files. The 'metadata' argument takes a sequence of strings
-    representing metadata. The 'projects' argument takes a sequence of project
-    names.
-
-    Any project that is not listed on PyPI will be considered ported.
-    """
-    dependencies = main.projects_from_requirements(requirements_paths)
-    dependencies.extend(main.projects_from_metadata(metadata))
-    dependencies.extend(projects)
-    dependencies = set(name.lower() for name in dependencies)
-
-    py3_projects = pypi.all_py3_projects()
-    all_projects = pypi.all_projects()
-
-    for dependency in dependencies:
-        if dependency in all_projects and dependency not in py3_projects:
-            return False
-    return True
